0bbcbd7df9ec2e68496ed99cf6531b1392885575
Laksan Nathan <laksannathan@outlook.com>
2019-12-23 15:01:58 +0100

    Init project

--- tesseract.py
+++ tesseract.py
@@ -1,2 +1,14 @@
 
 # This is a comment
+
+# %%
+from PIL import Image, ImageEnhance, ImageFilter
+import pytesseract
+path = 'data/example1.png'
+img = Image.open(path)
+pix = img.load()
+
+text = pytesseract.image_to_string(Image.open('data/temp.jpg'))
+print(text)
+
+# %%
